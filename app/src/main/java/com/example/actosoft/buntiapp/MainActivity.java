1b05833ccdf4ae96219d577cb1658741563cf0b1
Emily Karelis Garcia Quichimbo <emilygarcia@Air-de-Emily.lan>
2019-07-09 14:45:01 -0500

    Anadi un nuevo archivo para prueba

--- app/src/main/java/com/example/actosoft/buntiapp/MainActivity.java
+++ app/src/main/java/com/example/actosoft/buntiapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.actosoft.buntiapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anuragdhunna.www.splashscreen.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
