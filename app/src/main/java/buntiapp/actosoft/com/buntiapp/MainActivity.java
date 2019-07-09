643e5ac3cce6fc1a98bc2bd8dcdfb36c28b2b4f7
Yagel <yagels.xd@gmail.com>
2019-07-09 15:59:24 -0500

    Realiza geolocalizacion en tiempo real y obtiene mejor provider para utilizar servicio

--- app/src/main/java/buntiapp/actosoft/com/buntiapp/MainActivity.java
+++ app/src/main/java/buntiapp/actosoft/com/buntiapp/MainActivity.java
@@ -3,13 +3,18 @@ package buntiapp.actosoft.com.buntiapp;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         localizacion();
+//        listaProviders();
+//        listaProviders();
+//        mejorCriterio();
+//        estadoGPS();
+        registrarLocalizacion();
     }
 
     private void localizacion() {
@@ -31,17 +41,83 @@ public class MainActivity extends AppCompatActivity {
             }, 1000);
         }
 
-        longitud = (TextView)findViewById(R.id.txtLongitud);
-        latitud = (TextView)findViewById(R.id.txtLatitud);
+        longitud = (TextView) findViewById(R.id.txtLongitud);
+        latitud = (TextView) findViewById(R.id.txtLatitud);
 
         ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location loc = ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        if(ubicacion!=null) {
+        if (ubicacion != null) {
             Log.d("Latitud", String.valueOf(loc.getLatitude()));
             Log.d("Longitud", String.valueOf(loc.getLongitude()));
-            longitud.setText("LONGITUD: " + String.valueOf(loc.getLongitude()));
-            latitud.setText("LATITUD: " + String.valueOf(loc.getLatitude()));
+//            longitud.setText("LONGITUD: " + String.valueOf(loc.getLongitude()));
+//            latitud.setText("LATITUD: " + String.valueOf(loc.getLatitude()));
+        }
+    }
+
+    private void listaProviders() {
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> listaProvider = ubicacion.getAllProviders();
+
+        String mejorProvider = ubicacion.getBestProvider(mejorCriterio(), false);
+        System.out.println(mejorProvider);
+
+        LocationProvider provider = ubicacion.getProvider(listaProvider.get(0));
+        System.out.println(provider.getAccuracy());
+        System.out.println(provider.getPowerRequirement());
+        System.out.println(provider.supportsAltitude());
+    }
+
+    private Criteria mejorCriterio() {
+        Criteria requerimiento = new Criteria();
+        requerimiento.setAccuracy(Criteria.ACCURACY_FINE);
+        requerimiento.setAltitudeRequired(true);
+        return requerimiento;
+    }
+
+    private boolean estadoGPS() {
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!ubicacion.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("GPS", "NO ACTIVADO");
+        } else {
+            Log.d("GPS", "ACTIVADO");
+        }
+        return true;
+    }
+
+    private void registrarLocalizacion() {
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        ubicacion.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, new miLocalizacionListener());
+    }
+
+    private class miLocalizacionListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            String lat = "Mi latitud es: " + location.getLatitude();
+            String lon = "Mi longitud es: " + location.getLongitude();
+            latitud.setText(lat);
+            longitud.setText(lon);
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
         }
     }
 
