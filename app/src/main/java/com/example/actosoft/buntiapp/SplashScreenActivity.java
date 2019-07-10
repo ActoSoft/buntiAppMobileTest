7853f46e58c81e27b11ce1594f227d218327615c
Emily Karelis Garcia Quichimbo <emilygarcia@Air-de-Emily.lan>
2019-07-10 10:02:10 -0500

    Realice el Splash Screen

---
+++ app/src/main/java/com/example/actosoft/buntiapp/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.actosoft.buntiapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anuragdhunna.www.splashscreen.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen2);
+
+
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent =
+                        new Intent(getApplicationContext(),
+                                MainActivity.
+                                        class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+
+    }
+}
